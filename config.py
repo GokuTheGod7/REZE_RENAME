e5e213e0accf1ce82b82884b948743e2a07274c9
GokuTheGod7 <anshganava.4444@gmail.com>
2025-09-27 03:45:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", 7414019)
     API_HASH  = os.environ.get("API_HASH", "d463ed3d695f5cd4164029405ad8388e")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8220312376:AAGvKjB8cRujRQqNdZ2xbUzzewY36gEL33k")
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","Zoro")
