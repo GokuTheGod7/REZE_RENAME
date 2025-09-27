141002b0119dc524ec8b6646df37f7948384dfcc
GokuTheGod7 <anshganava.4444@gmail.com>
2025-09-27 19:12:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,8 +16,8 @@ class Config(object):
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://envs.sh/GNk.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7793156995 1352449904').split()]
-    FORCE_SUB_1 = os.environ.get("FORCE_SUB_1", "https://t.me/Eternal_Anime_chat")
-    FORCE_SUB_2 = os.environ.get("FORCE_SUB_2", "https://t.me/Eternal_society")
+    FORCE_SUB_1 = os.environ.get("FORCE_SUB_1", "-1002949135839")
+    FORCE_SUB_2 = os.environ.get("FORCE_SUB_2", "-1001958478348")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1002918784392))
     DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1002918784392"))
 
